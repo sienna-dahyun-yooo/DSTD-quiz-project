70e7bc38f37e751c01698199c6c205c8a319bf50
sienna-dahyun-yoo <yoos2@stu.otc.school.nz>
2026-05-01 09:59:14 +1200

    added the list of questions

---
+++ project.py
@@ -0,0 +1,36 @@
+''' a prgram that gives user questions about Lisa Carrington'''
+question_list=[
+    
+    {'question': 'What year was she born?',
+     'options': 'A.1983  B.1989  C.2001  D.1994',
+     'answer':'B'},
+
+    {'question': 'How many olimpic gold medal did she gain?',
+     'options': 'A.5  B.6  C.7  D.8,',
+     'answer':'D'},
+
+    {'question': 'What is her birthday?',
+     'options': 'A.November 8  B.September 13  C.June 23  D.May 1',
+     'answer':'C'},
+
+    {'question': 'What sports does she play?',
+     'options': 'A.Canoe   B.Badminton  C. Table tennis  D.Rock climbing',
+     'answer':'A'},
+
+    {'question': 'What year did she get her first Olimpic gold medal?',
+     'options': 'A.2020 Tokyo   B.2016 Rio de Janeiro  C.2012 London  D. 2008 Beijing',
+     'answer':'C'},
+
+    {'question': 'What place was she born?',
+     'options': 'A. Auckland  B.Tauranga  C.Christchurch  D.Dunedin,',
+     'answer':'B'},
+
+    {'question': 'What year did she get her first Olimpic medal?',
+     'options': 'A.2012   B.2008  C.2016  D.2020,',
+     'answer':'A'},
+    
+    {'question': 'What is the name of her partner?',
+     'options': 'A. George Bell  B. Ben Waine  C. Jayden Movold  D.Michael Buck,',
+     'answer':'D'},
+
+]
