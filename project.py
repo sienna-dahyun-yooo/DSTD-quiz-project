b443e9c061272aa57b130d8673fc0ea30d2b0bf7
sienna-dahyun-yoo <yoos2@stu.otc.school.nz>
2026-05-05 10:58:55 +1200

    added input for the quiz

--- project.py
+++ project.py
@@ -1,43 +1,45 @@
 ''' a prgram that gives user questions about Lisa Carrington'''
 #questions in the game
-question=[ 
+question_list=[ 
     {'question': 'What year was she born?',
-     'options': 'A.1983  B.1989  C.2001  D.1994',
+     'option': ['A.1983  B.1989  C.2001  D.1994'],
      'answer':'B'},
 
     {'question' : 'How many olimpic gold medal did she gain?',
-     'options' : 'A.5  B.6  C.7  D.8,',
+     'option' : ['A.5  B.6  C.7  D.8,'],
      'answer' :'D'},
 
     {'question': 'What is her birthday?',
-     'options': 'A.November 8  B.September 13  C.June 23  D.May 1',
+     'option': ['A.November 8  B.September 13  C.June 23  D.May 1'],
      'answer':'C'},
 
     {'question': 'What sports does she play?',
-     'options': 'A.Canoe   B.Badminton  C. Table tennis  D.Rock climbing',
+     'option': ['A.Canoe   B.Badminton  C. Table tennis  D.Rock climbing'],
      'answer':'A'},
 
     {'question': 'What year did she get her first Olimpic gold medal?',
-     'options': 'A.2020 Tokyo   B.2016 Rio de Janeiro  C.2012 London  D. 2008 Beijing',
+     'option': ['A.2020 Tokyo   B.2016 Rio de Janeiro  C.2012 London  D. 2008 Beijing'],
      'answer':'C'},
 
     {'question': 'What place was she born?',
-     'options': 'A. Auckland  B.Tauranga  C.Christchurch  D.Dunedin,',
+     'option': ['A. Auckland  B.Tauranga  C.Christchurch  D.Dunedin,'],
      'answer':'B'},
 
     {'question': 'What year did she get her first Olimpic medal?',
-     'options' : 'A.2012   B.2008  C.2016  D.2020,',
+     'option' : ['A.2012   B.2008  C.2016  D.2020,'],
      'answer' :'A'},
     
     {'question': 'What is the name of her partner?',
-     'options' : 'A. George Bell  B. Ben Waine  C. Jayden Movold  D.Michael Buck,',
+     'option' : ['A. George Bell  B. Ben Waine  C. Jayden Movold  D.Michael Buck,'],
      'answer' :'D'},
 ]
 
 #asking user if they want to start the quiz
 start=input('Do you want to start a quiz about Lisa Carrington?(Yes/No)\n')
 if start == "yes":
-    for question in question:
+    score=0
+    for question in question_list:
         print(question['question'])
-    #for options in question:
-        #print(question['options'])
+        for option in question['option']:
+            print(option)
+        user_input=input('Enter the anwser you think is right\n')
