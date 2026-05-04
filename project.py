ce2e5d59a492cafb29b21b8bb583106f407d064e
sienna-dahyun-yoo <yoos2@stu.otc.school.nz>
2026-05-04 13:26:19 +1200

    asking user to do quiz & print quiz

--- project.py
+++ project.py
@@ -1,13 +1,13 @@
 ''' a prgram that gives user questions about Lisa Carrington'''
-question_list=[
-    
+#questions in the game
+question=[ 
     {'question': 'What year was she born?',
      'options': 'A.1983  B.1989  C.2001  D.1994',
      'answer':'B'},
 
-    {'question': 'How many olimpic gold medal did she gain?',
-     'options': 'A.5  B.6  C.7  D.8,',
-     'answer':'D'},
+    {'question' : 'How many olimpic gold medal did she gain?',
+     'options' : 'A.5  B.6  C.7  D.8,',
+     'answer' :'D'},
 
     {'question': 'What is her birthday?',
      'options': 'A.November 8  B.September 13  C.June 23  D.May 1',
@@ -26,11 +26,18 @@ question_list=[
      'answer':'B'},
 
     {'question': 'What year did she get her first Olimpic medal?',
-     'options': 'A.2012   B.2008  C.2016  D.2020,',
-     'answer':'A'},
+     'options' : 'A.2012   B.2008  C.2016  D.2020,',
+     'answer' :'A'},
     
     {'question': 'What is the name of her partner?',
-     'options': 'A. George Bell  B. Ben Waine  C. Jayden Movold  D.Michael Buck,',
-     'answer':'D'},
-
+     'options' : 'A. George Bell  B. Ben Waine  C. Jayden Movold  D.Michael Buck,',
+     'answer' :'D'},
 ]
+
+#asking user if they want to start the quiz
+start=input('Do you want to start a quiz about Lisa Carrington?(Yes/No)\n')
+if start == "yes":
+    for question in question:
+        print(question['question'])
+    #for options in question:
+        #print(question['options'])
